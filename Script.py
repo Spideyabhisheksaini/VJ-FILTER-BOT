00e449c0dfb860258f4de39e34577f76c27d2d57
Spideyabhisheksaini <sainiabhi310@gmail.com>
2024-05-18 13:44:30 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -681,11 +681,11 @@ Also your tutorial will be Added Your Group..."""
     DEVELOPER_TXT = """
 special Thanks To ❤️ Developers -
 
--Dev 1 [Owner of this bot ]<a href='https://t.me/KingVj01'>VJ</a>
+-Dev 1 [Owner of this bot ]<a href='https://t.me/Tony_Stark_75'>ᴛᴏɴʏ sᴛᴀʀᴋ</a>
 
--Dev 2 <a href='https://t.me/vjbots_bot'>VJ</a>
+-Dev 2 <a href='https://t.me/Tonystark_botz'>ᴛᴏɴʏ sᴛᴀʀᴋ</a>
 
--Dev 3 <a href='https://t.me/vj_botz>VJ Botz</a>
+-Dev 3 <a href='https://t.me/Tonystark_botz'>ᴛᴏɴʏ sᴛᴀʀᴋBotz</a>
 
 - Dev 4 <a href='https://t.me/vj_bots'>TEAM VJ</a>
 """
